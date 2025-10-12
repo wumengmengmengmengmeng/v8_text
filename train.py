e6fd83015aa35e7a722d8beba21a0976732994ad
wumengmeng <570719593@qq.com>
2025-10-12 20:31:12 +0800

    yolov8_text1

---
+++ train.py
@@ -0,0 +1,23 @@
+import time
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    start_time = time.time()
+    
+
+    model = YOLO("./yolov8n.pt")  # 可以改成你的本地预训练权重路径
+
+   
+    results = model.train(
+        data='data.yaml',
+        epochs=1,
+        imgsz=640,
+        workers=0,
+        batch=-1,
+        device='',
+        resume=False,
+        project='runs/train',
+        name='exp2',  # 注意和你要读取的 best.pt 对应
+        single_cls=False,
+        cache=True
+    )
